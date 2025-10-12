d21dc3eaa0b58bb1e63d862e032ee5497cb7e6d6
ilnaz <chizescrap@mail.ru>
2025-10-12 19:04:40 +0300

    added setuf file

---
+++ ml/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='ml',
+   version='1.0.0',
+   description='A useful module',
+   install_requires=[],
+   author='Ilnaz',
+   author_email='chiezescrap@mail.ru',
+   packages=['src', 'models']
+)

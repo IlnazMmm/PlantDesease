2d18582c8b2fc3f63c0be600d6df4620224cca87
ilnaz <chizescrap@mail.ru>
2025-10-28 05:56:06 +0300

    Added nginx conf

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const defaultBaseUrl =
-  process.env.NODE_ENV === "production" ? "/api" : "http://localhost:8000";
+  process.env.NODE_ENV === "production" ? "" : "http://localhost:8000";
 
 export const API_BASE_URL = process.env.REACT_APP_API_URL ?? defaultBaseUrl;
 
